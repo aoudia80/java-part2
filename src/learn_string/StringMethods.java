91a8d54e6d73d911fe10ac99aecc4a041d20dc1f
aoudia80 <aoudia80@yahoo.fr>
2024-09-19 00:14:46 -0400

    adding some other methods

--- src/learn_string/StringMethods.java
+++ src/learn_string/StringMethods.java
@@ -3,7 +3,19 @@ package learn_string;
 public class StringMethods {
     public static void main(String[] args) {
         String str= "aksil";
+        String str2="d";
         char strIndex=str.charAt(3);
         System.out.println(strIndex);
+        // substring method
+        String subString=str.substring(1,4);
+        System.out.println(subString);
+
+        //equal method
+        Boolean isEqual=str.equals(str2);
+        System.out.println(isEqual);
+
+        //equal empty
+        Boolean isEmpty=str2.isEmpty();
+        System.out.println(isEmpty);
     }
 }
