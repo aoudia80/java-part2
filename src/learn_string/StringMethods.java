e77cdb49cb7a2e5b25fbc01066b08bc223a77ef4
aoudia80 <aoudia80@yahoo.fr>
2024-09-19 00:03:18 -0400

    methods

---
+++ src/learn_string/StringMethods.java
@@ -0,0 +1,9 @@
+package learn_string;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String str= "aksil";
+        char strIndex=str.charAt(3);
+        System.out.println(strIndex);
+    }
+}
