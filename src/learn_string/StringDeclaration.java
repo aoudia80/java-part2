34a62e15e10bd5eddc9a61a084194b97455bc553
aoudia80 <aoudia80@yahoo.fr>
2024-09-18 23:53:20 -0400

    string concatenation

--- src/learn_string/StringDeclaration.java
+++ src/learn_string/StringDeclaration.java
@@ -1,7 +1,19 @@
 package learn_string;
 
+import java.net.StandardSocketOptions;
+import java.util.Locale;
+
 public class StringDeclaration {
     public static void main(String[] args) {
-        String str="";
+        String str1="hello";
+
+        String str2="student";
+        // the length method
+        int strLength=str1.length();// it will return the number of character
+        System.out.println(strLength);
+
+        //concatenation method
+       String strConcat= str1.concat(""+str2);
+       System.out.println(strConcat);
     }
 }
